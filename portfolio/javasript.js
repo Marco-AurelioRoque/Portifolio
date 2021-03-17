c0133a5cc9dfa8564004fa5abe455b76366efe14
Marco Aurélio Roque de Oliveira Junior <marcoaureliocraft@gmail.com>
2021-03-17 12:32:35 -0300

    atualizaçao...

--- portfolio/javasript.js
+++
@@ -1,8 +0,0 @@
-document.querySelector('#items')
-.addEventListener('wheel', function (Event) {
-        if (Event.deltaY > 0) {
-            Event.target.scrolby(600, 0)
-        } else {
-            Event.target.scrolby(-600, 0)
-        }
-    })
